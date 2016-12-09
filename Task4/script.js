907ea59fb8956429b5a6d2bf6afea67294c49ab0
Lina25 <lina.kurylo@gmail.com>
2016-12-10 00:00:07 +0300

    add task4

---
+++ Task4/script.js
@@ -0,0 +1,12 @@
+var symbols = "# # # # #";
+var symbols2 = " # # # # #";
+var i = 0;
+while (i < 7) {
+    if (i % 2 == 0) {
+        console.log(symbols);
+    }
+    else {
+        console.log(symbols2);
+    }
+    i++
+}
