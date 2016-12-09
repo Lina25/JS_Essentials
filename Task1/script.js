2c658a67870ddb908b772a78373e529b33fa75a4
Lina25 <lina.kurylo@gmail.com>
2016-12-09 18:18:29 +0300

    add task1

---
+++ Task1/script.js
@@ -0,0 +1,5 @@
+var name = prompt("Please enter name", "Kate");
+var time = prompt("Please enter time", "09.00");
+var place = prompt("Please enter place", "Lviv");
+var event = prompt("Please enter event", "buy smth");
+console.log(name + " will " + event + " at " + time + " in " + place);
